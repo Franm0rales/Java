9a692e88e3f284512a33fc051e560d8a40356e57
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-03-16 19:38:12 +0100

    Metodos

---
+++ Metodos.java
@@ -0,0 +1,14 @@
+public class Metodos {
+    public static void main(String[] args) {
+        playMovie();
+        pauseMovie();
+    }
+
+    public static void playMovie() {
+        System.out.println("Playing movie...");
+    }
+
+    public static void pauseMovie() {
+        System.out.println("Paused movie...");
+    }
+}
